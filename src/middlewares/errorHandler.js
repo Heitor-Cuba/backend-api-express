5f9e8006e0a3dd0fa230c99f189f778d3e2eb269
Heitor-Cuba <heitorcuba@gmail.com>
2026-05-19 19:33:23 -0300

    Middleware Atualizado

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,14 @@
 export const errorHandler = (error, req, res, next) => {
-    console.lof(error.message)
+    console.log(error.message)
     console.log(error.name)
 
-    if (error?.message?.includes("in JSON at position")){
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                message: "Erro no servidor, por favor tente novamente mais tarde."
+    if(error?.message?.includes("in JSON at position")){
+        return res.status(500).json({
+            message: "Erro de sintaxe no JSON. Por favor verifique o formato do JSON enviado."
+        })
     }
 
-    res.status(500).json({
-        message: "Erro no servidor, por favor tente novamente mais tarde."
+    return res.status(500).json({
+        message: "Erro no servidor. Por favor tente novamente mais tarde."
     })
 }
