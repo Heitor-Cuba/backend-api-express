f5e9beb4b3f1731094ac0f745bd1a29d9138bb7d
Heitor-Cuba <heitorcuba@gmail.com>
2026-05-19 19:34:09 -0300

    Model Atualizados

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,19 +3,17 @@ import {prisma} from '../helpers/dbConnection.js'
 import { createValidator } from '../helpers/createValidator.js'
 
 const userSchema = z.object({
-    id: z.number("ID é obrigatório e deve ser um valor numérico")
-        .positive("ID deve ser um valor numérico positivo"),
-    avatar: z.string()
-            .url("Avatar é obrigatório e deve ser uma URL válida")
-            .max(500, "Avatar deve ter no máximo 500 caracteres"),
+    id: z.int("Id é obrigatório e deve ser um valor numérico")
+      .positive("Id deve ser um valor numérico positivo"),
+    avatar: z.url("Avatar deve ser uma URL válida")
+      .max(500, "Avatar deve ter no máximo 500 caracteres"),
     name: z.string("Nome deve ser uma string")
-          .min(3, "Nome deve ter no mínimo 3 caracteres")
-          .max(255, "Nome deve ter no máximo 255 caracteres"),
-    email: z.string()
-           .email("Email deve ser um endereço de email válido"),
+      .min(3, "Nome deve ter no mínimo 3 caracteres")
+      .max(255, "Nome deve ter no máximo 255 caracteres"),
+    email: z.email("Email deve ser um endereço de email válido"),
     pass: z.string("Senha é obrigatória e deve ser uma string")
-          .min(6, "Senha deve ter no mínimo 6 caracteres")
-          .max(255, "Senha deve ter no máximo 255 caracteres")
+      .min(6, "Senha deve ter no mínimo 6 caracteres")
+      .max(255, "Senha deve ter no máximo 255 caracteres")
 })
 
 export const validateUser = createValidator(userSchema)
@@ -26,10 +24,18 @@ export const createUser = async (user) => {
     })
 }
 
-export const getUsers = async () => {
-    return await prisma.user.findMany()
+export const getUsers = async (name) => {
+    return await prisma.user.findMany(
+        name ? { 
+            where: { 
+                name: { 
+                    contains: name
+                }
+            } 
+        } : {}
+    )
 }
-
+  
 export const deleteUser = async (id) => {
     return await prisma.user.delete({
         where: {
