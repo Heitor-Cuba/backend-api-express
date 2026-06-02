6715c02f551f2fcfc76cc7fe6e68fc1544f0d85f
Heitor-Cuba <heitorcuba@gmail.com>
2026-06-02 19:13:19 -0300

    Models

--- src/models/sessionModel.js
+++ src/models/sessionModel.js
@@ -1,10 +1,28 @@
-import { prisma } from "../helpers/dbConnection.js";
+import { prisma } from '../helpers/dbConnection.js'
 
 export const createSession = async (userId, token) => {
-    return await prisma.selection.session.create({
+    return await prisma.session.create({
         data: {
             userId,
             token
+        }
+    })   
+}
+
+export const getSessionByToken = async (token, userId) => {
+    return await prisma.session.findUnique({
+        where: {
+            token,
+            userId
+        }
+    })
+}
+
+export const deleteSession = async (token, userId) => {
+    return await prisma.session.delete({
+        where: {
+            token,
+            userId
         }
     })
 }
