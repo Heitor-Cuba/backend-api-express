836b3320d4aaa8aaf51c87d722ea61de08eb49f2
Heitor-Cuba <heitorcuba@gmail.com>
2026-06-02 19:16:55 -0300

    Resto

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
-//const express = require('express')
 import express from 'express'
 import userRouter from './routers/userRouter.js'
-import pubRouter from './routers/pubRouter.js'
+import publicationRouter from './routers/publicationRouter.js'
+import authRouter from './routers/authRouter.js'
 import { logger } from './middlewares/logger.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import { error404 } from './middlewares/error404.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
-const port = 3000
+const port = 3333
 
 app.use(logger)
+app.use(cookieParser())
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -17,7 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/user', userRouter)
-app.use('/pub', pubRouter)
+app.use('/publication', publicationRouter)
 app.use('/auth', authRouter)
 
 app.use(error404)
